427a92e6a4fb36ad635870e815af3f36d1a58dae
infoengine1337 <103350279+infoengine1337@users.noreply.github.com>
2023-02-12 08:58:49 +0900

    resolve glibc error

--- build.sh
+++ build.sh
@@ -535,8 +535,6 @@ make_packages_repo() {
     # Install packages on airootfs
     _pacstrap "${_pkglist_install[@]}"
 
-    _chroot_run "ldconfig"
-
     return 0
 }
 
@@ -680,7 +678,7 @@ make_setup_mkinitcpio() {
 
     sed -i "s|%COWSPACE%|${cowspace}|g" "${airootfs_dir}/etc/initcpio/hooks/archiso"
     #sed -i "s|/usr/lib/initcpio/|/etc/initcpio/|g" "${airootfs_dir}/etc/initcpio/install/archiso_shutdown"
-    install -m 0644 -- "${script_path}/system/initcpio/install/archiso_kms" "${airootfs_dir}/etc/initcpio/install"
+    #install -m 0644 -- "${script_path}/system/initcpio/install/archiso_kms" "${airootfs_dir}/etc/initcpio/install"
     #install -m 0755 -- "${script_path}/system/initcpio/script/archiso_shutdown" "${airootfs_dir}/etc/initcpio"
     install -m 0644 -- "${script_path}/mkinitcpio/mkinitcpio-archiso.conf" "${airootfs_dir}/etc/mkinitcpio-archiso.conf"
     [[ "${boot_splash}" = true ]] && cp "${script_path}/mkinitcpio/mkinitcpio-archiso-plymouth.conf" "${airootfs_dir}/etc/mkinitcpio-archiso.conf"
