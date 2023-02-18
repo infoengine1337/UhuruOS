ef9f86fa16afd414a34889bae865068e10278d13
infoengine1337 <103350279+infoengine1337@users.noreply.github.com>
2023-02-18 21:23:41 +0900

    block ipv6 connection

--- modules/obscuring/airootfs.any/root/customize_airootfs_obscuring.sh
+++ modules/obscuring/airootfs.any/root/customize_airootfs_obscuring.sh
@@ -5,7 +5,7 @@
 # Twitter: @infoengine1337
 
 _safe_systemctl mask systemd-timesyncd.service
-_safe_systemctl enable iptables.service macspoof.service NetworkManager.service dnsmasq.service secure-time-sync.service kloak.service
+_safe_systemctl enable iptables.service ip6tables.service macspoof.service NetworkManager.service dnsmasq.service secure-time-sync.service kloak.service
 #_safe_systemctl mask tor.service
 
 # Tor Initializer
